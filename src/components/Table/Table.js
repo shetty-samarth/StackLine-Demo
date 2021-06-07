b096fe35a3bff7e8de515490eec0c36e62b3c7ad
shettysam93 <aquasam93@gmail.com>
2021-06-07 12:01:36 -0400

    deployment 1.2

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -41,10 +41,10 @@ const Table = () =>{
                             <tr {...row.getRowProps}>
                                 {
                                     row.cells.map(cell =>{
-                                       return <td {...cell.getCellProps}>{cell.render('Cell')}</td>
+                                       return (<td {...cell.getCellProps}>{cell.render('Cell')}</td>
+                                       )
                                     })
                                 }
-                                
                             </tr>
                         )
                     })
